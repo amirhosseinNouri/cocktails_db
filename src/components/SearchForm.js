88ddbb5bfb8f518536fe602c626de5043db96c8b
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 15:56:52 +0330

    SearchForm done

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,29 @@
 import React from "react";
 
-export default function SearchForm() {
-  return <h1>searchform component</h1>;
+export default function SearchForm({setSearchTerm}) {
+
+  const searchValue = React.useRef('')
+
+  React.useEffect(() =>{
+    searchValue.current.focus()
+  },[])
+
+
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+  }
+
+  const searchCocktail = () =>{
+   setSearchTerm(searchValue.current.value)
+  }
+
+  return <section className="section">
+    <h2 className="section-title">Search Cocktails</h2>
+    <form className="form-search" onSubmit={handleSubmit}>
+      <div className="form-control">
+        <label htmlFor="name">Search Your favorite cocktail</label>
+        <input type="text" name="name" id="id" onChange={searchCocktail} ref={searchValue}  />
+      </div>
+    </form>
+  </section>;
 }
