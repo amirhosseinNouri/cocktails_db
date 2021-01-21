d53a7864d09a8b315af1504f9712cb8ccdae5bd5
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 15:26:47 +0330

    useEffect of SingleCocktail componenet done

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,7 +1,61 @@
+import axios from "axios";
 import React from "react";
-import {useParams} from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
 export default function SingleCocktail(props) {
-  const {id} = useParams()
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await axios.get(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+        );
+        const { drinks } = response.data;
+        console.log(drinks);
+        if (drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic : info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getCocktail();
+  }, [id]);
   return <h1>single cocktail page {id}</h1>;
 }
