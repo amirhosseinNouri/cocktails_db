b44cd1c3b3e3f0b77bdbe67bcf59ff4e203ca389
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 13:46:00 +0330

    CocktailList component done

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,6 +1,24 @@
 import React from "react";
+import Cocktail from "./Cocktail";
 
-export default function CocktailList({cocktails}) {
-  console.log(cocktails);
-  return <h1>cocktailList component</h1>;
+export default function CocktailList({ cocktails, loading }) {
+  if (loading) {
+    return <h2 className="section-title">Loading...</h2>;
+  }
+  if (cocktails.length === 0) {
+    return (
+      <h2 className="section-title">No Cocktails matched your search term</h2>
+    );
+  }
+
+  return (
+    <section className="section">
+      <h2 className="section-title">Cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => (
+          <Cocktail key={item.id} {...item}></Cocktail>
+        ))}
+      </div>
+    </section>
+  );
 }
