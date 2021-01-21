26038b2b220bd85695989cf9604771f56c462701
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 13:07:37 +0330

    set states in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
 import React from "react";
+import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
-  return <h1>Home page</h1>;
+  const [loading, setLoading] = React.useState(false);
+  const [searchTerm, setSearchTerm] = React.useState("a");
+  const [cocktails, setCocktails] = React.useState();
+  return (
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm}></SearchForm>
+      <CocktailList cocktails={cocktails} loading={loading}></CocktailList>
+    </main>
+  );
 }
