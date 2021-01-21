194c6ad7cf0b1983c733e64b062a83fb749f0914
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 16:05:11 +0330

    Unused Import removed

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React from "react";
 import { useParams, Link } from "react-router-dom";
-import Cocktail from "../components/Cocktail";
 
 export default function SingleCocktail(props) {
   const { id } = useParams();
