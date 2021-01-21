33c52052b4fc66c32da7a221e4b5692d85c36fff
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 15:58:34 +0330

    SearchForm component  formatted

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,29 +1,35 @@
 import React from "react";
 
-export default function SearchForm({setSearchTerm}) {
+export default function SearchForm({ setSearchTerm }) {
+  const searchValue = React.useRef("");
 
-  const searchValue = React.useRef('')
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
 
-  React.useEffect(() =>{
-    searchValue.current.focus()
-  },[])
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
 
-  const handleSubmit = (event) =>{
-    event.preventDefault()
-  }
-
-  const searchCocktail = () =>{
-   setSearchTerm(searchValue.current.value)
-  }
-
-  return <section className="section">
-    <h2 className="section-title">Search Cocktails</h2>
-    <form className="form-search" onSubmit={handleSubmit}>
-      <div className="form-control">
-        <label htmlFor="name">Search Your favorite cocktail</label>
-        <input type="text" name="name" id="id" onChange={searchCocktail} ref={searchValue}  />
-      </div>
-    </form>
-  </section>;
+  return (
+    <section className="section">
+      <h2 className="section-title">Search Cocktails</h2>
+      <form className="form-search" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Search Your favorite cocktail</label>
+          <input
+            type="text"
+            name="name"
+            id="id"
+            onChange={searchCocktail}
+            ref={searchValue}
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
