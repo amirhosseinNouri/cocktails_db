f41743b436d7a3e99a8b86bbfa8af978827740d8
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 13:26:08 +0330

    useEffect added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
 import CocktailList from "../components/CocktailList";
 import SearchForm from "../components/SearchForm";
+import axios from "axios";
 
 export default function Home() {
   const [loading, setLoading] = React.useState(false);
   const [searchTerm, setSearchTerm] = React.useState("a");
-  const [cocktails, setCocktails] = React.useState();
+  const [cocktails, setCocktails] = React.useState([]);
+
+  React.useEffect(async () => {
+    const data = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+    );
+    setCocktails(data.data.drinks)
+  }, [searchTerm]);
   return (
     <main>
       <SearchForm setSearchTerm={setSearchTerm}></SearchForm>
